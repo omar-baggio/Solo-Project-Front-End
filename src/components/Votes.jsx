ea100a9316669bc9064af5ca2283e49468a691a8
omar-baggio <oomarali1000@gmail.com>
2023-07-19 11:50:19 +0100

    addressed comments for ticket 7

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -5,28 +5,19 @@ const Votes = ({ newArticle }) => {
   const originalVote = newArticle.votes;
   const [newVotes, setNewVotes] = useState(originalVote);
   const [voteChange, setVoteChange] = useState(0);
+  const [err, setErr] = useState(null);
 
-  const increase = () => {
+  const updateVotes = (changeValue) => {
     if (voteChange === 0) {
-      patchArticle(newArticle.article_id, 1);
-      setNewVotes(newVotes + 1);
-      setVoteChange(1);
-    } else if (voteChange === 1) {
-      patchArticle(newArticle.article_id, -1);
-      setNewVotes(newVotes - 1);
+      patchArticle(newArticle.article_id, changeValue);
+      setNewVotes(newVotes + changeValue);
+      setVoteChange(changeValue);
+      setErr(null);
+    } else if (voteChange === changeValue) {
+      patchArticle(newArticle.article_id, -changeValue);
+      setNewVotes(newVotes - changeValue);
       setVoteChange(0);
-    }
-  };
-
-  const decrease = () => {
-    if (voteChange === 0) {
-      patchArticle(newArticle.article_id, -1);
-      setNewVotes(newVotes - 1);
-      setVoteChange(-1);
-    } else if (voteChange === -1) {
-      patchArticle(newArticle.article_id, 1);
-      setNewVotes(newVotes + 1);
-      setVoteChange(0);
+      setErr("Something went wrong, please try again.");
     }
   };
 
@@ -36,12 +27,12 @@ const Votes = ({ newArticle }) => {
   return (
     <div className="votesBox">
       <p className="vote">Vote</p>
-      <button onClick={() => increase(1)} disabled={voteChange === -1}>
+      <button onClick={() => updateVotes(1)} disabled={voteChange === -1}>
         {" "}
         +{" "}
       </button>
       {votingText}
-      <button onClick={() => decrease(-1)} disabled={voteChange === 1}>
+      <button onClick={() => updateVotes(-1)} disabled={voteChange === 1}>
         {" "}
         -{" "}
       </button>
